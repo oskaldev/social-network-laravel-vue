f60e133580a862485341a3ec5402c3d17c97ab49
oskaldev <artem-osipovartem3002@mail.ru>
2025-05-17 20:38:56 +0300

    add prepareLikedPOsts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Post\PostResourse;
 use App\Http\Resources\User\UserResource;
+use App\Models\LikedPost;
 use App\Models\Post;
 use App\Models\SubscriberFollowing;
 use App\Models\User;
@@ -36,6 +37,9 @@ class UserController extends Controller
      */
     public function post(User $user)
     {
+        $posts = $user->posts()->latest()->get();
+        $posts = $this->prepareLikedPOsts($posts);
+
         return PostResourse::collection($user->posts);
     }
 
@@ -56,9 +60,32 @@ class UserController extends Controller
      */
     public function followingPost(): AnonymousResourceCollection
     {
-        $followedIds = auth()->user()->followings()->get()->pluck('id')->toArray();
+        $followedIds = auth()->user()->followings()->latest()->get()->pluck('id')->toArray();
         $posts = Post::whereIn('user_id', $followedIds)->get();
+        $likedIds = LikedPost::where('user_id', auth()->id())
+            ->get('post_id')->pluck('post_id')->toArray();
+
+        $posts = Post::whereIn('user_id', $followedIds)
+            ->whereNotIn('id', $likedIds)
+            ->get();
+
+        $posts = $this->prepareLikedPOsts($posts);
 
         return PostResourse::collection($posts);
+    }
+
+    private function prepareLikedPOsts($posts): mixed
+    {
+
+        $likedIds = LikedPost::where('user_id', auth()->id())
+            ->get('post_id')->pluck('post_id')->toArray();
+
+        foreach ($posts as $post) {
+            if (in_array($post->id, $likedIds)) {
+                $post->is_liked = true;
+            }
+        }
+
+        return $posts;
     }
 }
